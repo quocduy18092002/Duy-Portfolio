49723f80c370e30d5b5b42842c5c24d19b46f563
quocduy18092002 <quocduy18092002@gmail.com>
2024-12-31 14:23:45 +0700

    fix: works

--- my-app/src/components/Works/Works.tsx
+++ my-app/src/components/Works/Works.tsx
@@ -17,19 +17,19 @@ const Works = () => {
         <div className="text-white md:font-semibold md:text-5xl font-normal text-3xl tracking-[.1rem] md:pt-8 normal-case">
           Here are some of the projects I've worked on recently. Feel free to take a look!
         </div> 
-        <div className="flex pt-10">
-          <Link href={"https://news-paper-eight.vercel.app/"} target="_blank" className="flex flex-row gap-3 w-[50%]">
-          <div><Image src={"/images/logo.jpg"} alt="" width={150} height={150}/></div>
-          <div>
-            <div className="text-gray-500 text-xs font-normal">Website</div>
-            <div className="text-white font-normal text-2xl tracking-normal">News Paper</div>
+        <div className="flex flex-col md:flex-row pt-10 gap-4 md:gap-0">
+          <Link href={"https://news-paper-eight.vercel.app/"} target="_blank" className="flex flex-row gap-3 md:w-[50%]">
+          <div><Image src={"/images/logo.jpg"} alt="" width={168} height={150}/></div>
+          <div className="md:border-t border-gray-500">
+            <div className="text-gray-500 text-xs font-normal pt-2">Website</div>
+            <div className="text-white font-normal md:text-2xl tracking-normal">News Paper</div>
           </div>
           </Link>
-           <Link href={"https://art-web-tau.vercel.app/"} target="_blank" className="flex flex-row gap-3 w-[50%]">
+           <Link href={"https://art-web-tau.vercel.app/"} target="_blank" className="flex flex-row gap-3 md:w-[50%]">
           <div><Image src={"/images/artlogo.jpg"} alt="" width={168} height={300}/></div>
-          <div>
-            <div className="text-gray-500 text-xs font-normal">Website</div>
-            <div className="text-white font-normal text-2xl tracking-wide">Canvastart Art</div>
+          <div className="md:border-t border-gray-500">
+            <div className="text-gray-500 text-xs font-normal pt-2">Website</div>
+            <div className="text-white font-normal md:text-2xl tracking-wide ">Canvastart Art</div>
           </div>
           </Link>
         </div>
